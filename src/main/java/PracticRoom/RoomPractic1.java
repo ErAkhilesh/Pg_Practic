e592179940354347c1f44d1bfdfd9ac93838c712
Akhilesh <116070506+ErAkhilesh@users.noreply.github.com>
2023-01-30 06:35:41 +0530

    Akhilesh Monday Morning commit2
    I have Some Changes in Morning.

--- src/main/java/PracticRoom/RoomPractic1.java
+++ src/main/java/PracticRoom/RoomPractic1.java
@@ -6,5 +6,7 @@ public class RoomPractic1 {
 	@Test
 	public void update() {
 		System.out.println("Education is better for Tomorrow");
+		System.out.println("Job is necessary for us");
+		System.out.println("I wake up in Morning quickly");
 	}
 }
