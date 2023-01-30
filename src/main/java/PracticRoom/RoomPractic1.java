9708ab7dcfb8493b18062a86c2fce9a0e976af3d
ErAkhilesh <gakhilesh090@gmail.com>
2023-01-30 06:22:53 +0530

    Monday Morning commit

---
+++ src/main/java/PracticRoom/RoomPractic1.java
@@ -0,0 +1,10 @@
+package PracticRoom;
+
+import org.testng.annotations.Test;
+
+public class RoomPractic1 {
+	@Test
+	public void update() {
+		System.out.println("Education is better for Tomorrow");
+	}
+}
---
+++ src/main/java/PracticRoom/RoomPractic2.java
@@ -0,0 +1,10 @@
+package PracticRoom;
+
+import org.testng.annotations.Test;
+
+public class RoomPractic2 {
+	@Test
+	public void create() {
+		System.out.println("Lone for Study");
+	}
+}
