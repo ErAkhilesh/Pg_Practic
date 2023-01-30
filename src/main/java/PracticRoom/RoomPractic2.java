0a669a2faa9385dc82821050f868d44e792690db
ErAkhilesh <gakhilesh090@gmail.com>
2023-01-30 06:51:48 +0530

    Akg

--- src/main/java/PracticRoom/RoomPractic2.java
+++ src/main/java/PracticRoom/RoomPractic2.java
@@ -6,5 +6,7 @@ public class RoomPractic2 {
 	@Test
 	public void create() {
 		System.out.println("Lone for Study");
+		System.out.println("Job is necessary for us 123");
+		System.out.println("I wake up in Morning quickly 123");
 	}
 }
