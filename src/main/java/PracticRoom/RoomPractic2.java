c27affac5ffbcb69137377df6ad8e7dba908d6c3
Akhilesh <116070506+ErAkhilesh@users.noreply.github.com>
2023-01-30 06:58:54 +0530

    90
    91

--- src/main/java/PracticRoom/RoomPractic2.java
+++ src/main/java/PracticRoom/RoomPractic2.java
@@ -8,5 +8,6 @@ public class RoomPractic2 {
 		System.out.println("Lone for Study");
 		System.out.println("Job is necessary for us (DEV)12345");
 		System.out.println("I wake up in Morning quickly (Dev)12345");
+		System.out.println("Mission Sussessfull");
 	}
 }
