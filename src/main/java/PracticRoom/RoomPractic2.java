b27fc21089f8cc44eb8c327a963ce6fc9af59341
Akhilesh <116070506+ErAkhilesh@users.noreply.github.com>
2023-01-30 06:56:36 +0530

    6Am
    6: 00Am

--- src/main/java/PracticRoom/RoomPractic2.java
+++ src/main/java/PracticRoom/RoomPractic2.java
@@ -6,7 +6,7 @@ public class RoomPractic2 {
 	@Test
 	public void create() {
 		System.out.println("Lone for Study");
-		System.out.println("Job is necessary for us (DEV)");
-		System.out.println("I wake up in Morning quickly (Dev)");
+		System.out.println("Job is necessary for us (DEV)12345");
+		System.out.println("I wake up in Morning quickly (Dev)12345");
 	}
 }
